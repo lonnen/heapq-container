558785074b884b5109cf30279701eb7e590fe11f
lonnen <chris.lonnen@gmail.com>
2024-06-14 15:34:41 -0700

    force "enumerate" into a list

--- tests/test_heapqueue.py
+++ tests/test_heapqueue.py
@@ -86,7 +86,7 @@ class TestHeapQueue:
         assert sorted_data == sorted(data)
 
     def test_tuple(self):
-        data = enumerate("abcde")
+        data = list(enumerate("abcde"))
         shuffle(data)
         assert data != sorted(data, key=lambda d: d[0])
         pq = HeapQueue(data)
