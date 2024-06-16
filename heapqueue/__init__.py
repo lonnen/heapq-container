deb51a19109e47d87d17230a71f8560dcfc51ed3
lonnen <chris.lonnen@gmail.com>
2024-06-15 20:37:57 -0700

    ruff lint fixups

--- heapqueue/__init__.py
+++ heapqueue/__init__.py
@@ -12,15 +12,15 @@ class HeapQueue(UserList):
     This module follows the python convention of both zero-indexing and pop returning
     the smallest item, not the largest (called a "min heap").
 
-    By wrapping UserList this integrates the methods of `heapq` and presents the 
+    By wrapping UserList this integrates the methods of `heapq` and presents the
     interface of a Collections-style data container type. `heap[0]` is the smallest
     item, and `heap.sort()` maintains the heap invariant! Note that iterating over the
     `heap` directly, say, in a list comprehension, will not return items in a sorted
     order. However, repeatedly popping the heap to exhaustion will.
 
-    Complex records may be wrapped in a tuple (with careful thought given to ties), 
-    where tuple elements will be compared in positional order. Records can also be 
-    wrapped in a custom object. Dataclasses work well for this. See the heapq library 
+    Complex records may be wrapped in a tuple (with careful thought given to ties),
+    where tuple elements will be compared in positional order. Records can also be
+    wrapped in a custom object. Dataclasses work well for this. See the heapq library
     documentation on the Priority Queue implementation for more, or look in this
     module's test suite for an example.
     """
